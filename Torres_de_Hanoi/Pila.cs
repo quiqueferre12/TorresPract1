01e8bf8ff5c756c2879dade1cd7da4bc9cca076f
Quiqueferre12 <quiqueferre12@gmail.com>
2021-03-08 19:53:20 +0100

    Pila hecho

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -9,34 +9,44 @@ namespace Torres_de_Hanoi
     class Pila
     {
         public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
+        
         public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
+        
+       
+        
         public List<Disco> Elementos { get; set; }
-        */
+        
 
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            
         }
 
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
         }
 
         public Disco pop()
         {
-            return null;
+            Disco aux = Elementos[Elementos.Count-1];
+            Elementos.RemoveAt(Elementos.Count-1);
+            Size--;
+            return aux;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if (Elementos.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
 
     }
